aaf562aedb0035e181232c6059049bf05c5f0fdd
smithwebtek <brad@smithwebtek.com>
2017-12-20 22:14:54 -0500

    refactor with arrow functions

--- reducer.js
+++ reducer.js
@@ -2,14 +2,17 @@ let state;
 
 function changeState(state = {count: 0}, action){
   switch (action.type) {
+
     case 'INCREASE_COUNT':
       return {count: state.count + 1};
 
     case 'ASDF': 
       alert('Mrs. McGillicuddy wants her pan back.')
+      return state;
 
     case 'ZXCV':
       document.getElementById('container').classList = 'blue'
+      return state;
 
     default:
       return state;
@@ -31,16 +34,8 @@ let button = document.getElementById('button');
 let button2 = document.getElementById('button2');
 let buttonBlue = document.getElementById('buttonBlue');
 
-button.addEventListener('click', function() {
-  dispatch({ type: 'INCREASE_COUNT' });
-});
+button.addEventListener('click', () => dispatch({ type: 'INCREASE_COUNT' }));
+button2.addEventListener('click', () => dispatch({ type: 'ASDF'}));
+buttonBlue.addEventListener('click', () => dispatch({ type: 'ZXCV' }));
 
-button2.addEventListener('click', function() {
-  dispatch({ type: 'ASDF' });
-});
-
-buttonBlue.addEventListener('click', function() {
-  dispatch({ type: 'ZXCV' });
-});
-
-dispatch({type: '@@INIT'})
+dispatch({type: '@@INIT'});
