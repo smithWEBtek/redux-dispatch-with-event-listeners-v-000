02f331e51fd1c7a11febab6c8be819d42f04988d
smithwebtek <brad@smithwebtek.com>
2017-12-20 22:03:24 -0500

    add buttons and css class

--- reducer.js
+++ reducer.js
@@ -4,8 +4,12 @@ function changeState(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1};
+
     case 'ASDF': 
       alert('Mrs. McGillicuddy wants her pan back.')
+
+    case 'ZXCV':
+      document.getElementById('container').classList = 'blue'
 
     default:
       return state;
@@ -25,6 +29,7 @@ function render(){
 //https://blog.garstasio.com/you-dont-need-jquery/events/#listening-for-events
 let button = document.getElementById('button');
 let button2 = document.getElementById('button2');
+let buttonBlue = document.getElementById('buttonBlue');
 
 button.addEventListener('click', function() {
   dispatch({ type: 'INCREASE_COUNT' });
@@ -32,6 +37,10 @@ button.addEventListener('click', function() {
 
 button2.addEventListener('click', function() {
   dispatch({ type: 'ASDF' });
+});
+
+buttonBlue.addEventListener('click', function() {
+  dispatch({ type: 'ZXCV' });
 });
 
 dispatch({type: '@@INIT'})
