53dd850115f96b599675d0bad7acd7c0d6f91bcc
smithwebtek <brad@smithwebtek.com>
2017-12-20 21:54:06 -0500

    add a button

--- reducer.js
+++ reducer.js
@@ -1,9 +1,37 @@
 let state;
+
 function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    case 'ASDF': 
+      alert('Mrs. McGillicuddy wants her pan back.')
+
+    default:
+      return state;
   }
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+ 
+function render(){
+  document.getElementById('container').textContent = state.count;
+}
+
+
+//https://blog.garstasio.com/you-dont-need-jquery/events/#listening-for-events
+let button = document.getElementById('button');
+let button2 = document.getElementById('button2');
+
+button.addEventListener('click', function() {
+  dispatch({ type: 'INCREASE_COUNT' });
+});
+
+button2.addEventListener('click', function() {
+  dispatch({ type: 'ASDF' });
+});
+
+dispatch({type: '@@INIT'})
